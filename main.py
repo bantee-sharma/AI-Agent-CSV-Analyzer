73e622f8caf20552f4cc68756da604af84a983b4
bantee-sharma <banteesharma9@gmail.com>
2025-05-14 21:48:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model = "gemini-2.0-flash")
+
+agent=create_csv_agent(llm,"Bank-Records.csv",verbose=True,allow_dangerous_code=True)
+
+
+
+ques = "Types of cards"
+res = agent.invoke(ques)
+print(res)
