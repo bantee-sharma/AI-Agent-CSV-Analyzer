beb1eb276efe2665f6f5bbeae52f5d7ae87d34a5
bantee-sharma <banteesharma9@gmail.com>
2025-06-01 20:33:25 +0530

    Create n.py

---
+++ n.py
@@ -0,0 +1,21 @@
+from langchain_experimental.agents import create_csv_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+
+agent = create_csv_agent(
+    llm=llm,
+    path="Bank-Records.csv",
+    verbose=True,
+    allow_dangerous_code=True
+)
+
+query = "total number of people from spain"
+response = agent.invoke(query)
+print(response)
